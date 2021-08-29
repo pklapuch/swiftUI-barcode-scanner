0c2a3d7431bfd3e2f437291322be539e2b7a1eff
pklapuch <mr.pawel.klapuch@gmail.com>
2021-08-29 14:37:37 -0500

    [DEV] source code

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Pawel Klapuch on 8/29/21.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
